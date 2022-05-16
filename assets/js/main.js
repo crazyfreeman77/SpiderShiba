13516a48673085967116f56e4e1ae51851d11211
crazyfreeman77 <104940633+crazyfreeman77@users.noreply.github.com>
2022-05-16 22:59:36 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,33 @@ $(function(){
 
   reset.call(marquee.find("div"));
 
+});
 
-  
 
+
+$('.team__slider').slick({
+ 
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  dots: true,
+  arrows: false
 
 });
+
+var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
